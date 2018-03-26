7dc725b7e24b4fc7f370248639269e488454ada4
01372153 <doctorq@sf-express.com>
2018-03-26 14:04:20 +0800

    update

--- zhizheng.go
+++ zhizheng.go
@@ -4,6 +4,10 @@ func main() {
     var a int = 10
     fmt.Printf("变量的地址:%x\n",&a)
     var ip *int
+
+    if(ip == nil) {
+        fmt.Printf("ip 变量的值: %d\n", ip  )
+    }
     ip = &a
     fmt.Printf("a 变量的地址是: %x\n", &a  )
 
