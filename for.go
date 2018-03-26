b5403f3c7ddab2c38edeeee0f9910652bcaccf36
01372153 <doctorq@sf-express.com>
2018-03-26 11:12:14 +0800

    update

---
+++ for.go
@@ -0,0 +1,17 @@
+
+package main
+import "fmt"
+func main() {
+	var C,c int //变量声明
+	C = 1
+	A: for C < 100 {
+		C++
+		for c = 2; c < C;c++ {
+			if C%c == 0 {
+				goto A
+			}
+		}
+		fmt.Println(C,"是素数")
+	}
+
+}
