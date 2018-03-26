9764326a318396ae225c11ce1636937fa02f4062
01372153 <doctorq@sf-express.com>
2018-03-26 11:59:41 +0800

    update

--- zhizheng.go
+++ zhizheng.go
@@ -3,4 +3,16 @@ import "fmt"
 func main() {
     var a int = 10
     fmt.Printf("变量的地址:%x\n",&a)
+    var ip *int
+    ip = &a
+    fmt.Printf("a 变量的地址是: %x\n", &a  )
+
+   /* 指针变量的存储地址 */
+   fmt.Printf("ip 变量储存的指针地址: %x\n", ip )
+
+   /* 使用指针访问值 */
+   fmt.Printf("*ip 变量的值: %d\n", *ip )
+
+    fmt.Printf("ip 变量的地址是: %x\n", &ip  )
+
 }
