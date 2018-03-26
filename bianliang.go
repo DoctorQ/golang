b131944f837a9a38a9c06f2ef243b8b4a0481d87
01372153 <doctorq@sf-express.com>
2018-03-26 11:51:12 +0800

    update

--- bianliang.go
+++ bianliang.go
@@ -13,4 +13,13 @@ func main() {
     g = a + b
     fmt.Printf("结果:a=%d,b=%d and c=%d\n",a,b,c)
     fmt.Printf("结果:a=%d,b=%d and g=%d\n",a,b,g)
+    g = sum(a,b)
+    fmt.Printf("main() 函数中g = %d\n",g)
+}
+
+
+func sum(a,b int) int {
+    fmt.Printf("sum() 函数中a = %d\n",a)
+    fmt.Printf("sum() 函数中b = %d\n",b)
+    return a + b
 }
