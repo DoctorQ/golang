3ac6e41f6e879b23616bb573314fa336f7da6f08
01372153 <doctorq@sf-express.com>
2018-04-26 10:40:37 +0800

    update

---
+++ qiepian.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+
+func main() {
+
+    var split []int
+    var slice1 []int = make([]int,10)
+    var slice2 := make([]int,10)
+    split := int {1,2,3}
+    slice
+}
