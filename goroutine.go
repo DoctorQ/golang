650fa0d0c2436dc3bb15678dfa7cdbd2f124d066
01372153 <doctorq@sf-express.com>
2018-05-10 17:44:51 +0800

    update

--- goroutine.go
+++ goroutine.go
@@ -22,7 +22,7 @@ func main() {
 	go func() {
 		defer wg.Done()
 		for count := 0; count < 3; count++ {
-			for char := 'a'; char < 'a'+26; char++ {
+			for char := 'A'; char < 'A'+26; char++ {
 				fmt.Printf("%c ", char)
 			}
 		}
