2a51a25af6cacd91c590cfc1a2f26534d83199e3
doctorq <doctorq@sf-express.com>
2019-03-18 11:40:33 +0800

    update

--- longest_substring.go
+++ longest_substring.go
@@ -1,14 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	str := "abcabcbb"
-	str1 := "bbbbb"
-	str2 := "pwwkew"
+	//str1 := "bbbbb"
+	//str2 := "pwwkew"
+	lengthOfLongestSubstring(str)
 
 }
 
 func lengthOfLongestSubstring(s string) int {
+
+	longest := ""
+	for i := range s {
+		if strings.Index(longest, string(s[i])) != -1 {
+			//如果位置小于1，将重复位置之前的所有字符全部剔除掉，
+			//如果位置大于1，不提出，保持不变,作为备选项
+			fmt.Println(fmt.Errorf("%c exists in %s", s[i], longest))
+			continue
+		}
+		longest = fmt.Sprintf("%s%c", longest, s[i])
+		fmt.Printf("longest=%s\n", longest)
+	}
+	return len(longest)
 
 }
